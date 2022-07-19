bbdbe0b45fc8af03e5d733c9359c7c11120929fb
mojtabagituser <m.heydarpanah@gmail.com>
2022-07-18 21:23:58 -0400

    add code to viewmodel branch final step

---
+++ app/src/main/java/com/example/trackrunning/ui/fragments/SetupFragment.kt
@@ -0,0 +1,7 @@
+package com.example.trackrunning.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.trackrunning.R
+
+class SetupFragment:Fragment(R.layout.fragment_setup) {
+}
